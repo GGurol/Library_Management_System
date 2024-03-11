e795796a49c3d9745b0f695dd52bea61e76b30fc
Tonetor777 <101365568+Tonetor777@users.noreply.github.com>
2024-03-11 16:47:13 -0300

    login and signup with crispy forms

--- LibraryManagement/LibraryManagement/settings.py
+++ LibraryManagement/LibraryManagement/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'library.apps.LibraryConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'library'
 ]
 
 MIDDLEWARE = [
