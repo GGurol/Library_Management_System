98361b534fc7ff2c7b2083c5a00182db034c9f44
Tonetor777 <101365568+Tonetor777@users.noreply.github.com>
2024-03-13 10:44:46 -0300

    Added allbook page and book details

--- LibraryManagement/library/urls.py
+++ LibraryManagement/library/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('home/', views.home_page , name='home'),
     path('' , include('django.contrib.auth.urls')), 
     path('sign-up/', views.sign_up , name='sign_up'),
-    path('logout/', views.logout, name='logout')
+    path('logout/', views.logout, name='logout'),
+    path ('all-books/' , views.all_books , name='all-books'),
+    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
 ]
