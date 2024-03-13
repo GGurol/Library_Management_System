4e60485f8826b4b65046f4c124ea0b6120386b09
Tonetor777 <101365568+Tonetor777@users.noreply.github.com>
2024-03-13 09:10:25 -0300

    registered models om admin

--- LibraryManagement/library/admin.py
+++ LibraryManagement/library/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User
+from .models import Book
+from .models import Borrow
+from .models import Review
+from .models import Genre
+
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Book)
+admin.site.register(Borrow)
+admin.site.register(Review)
+admin.site.register(Genre)
